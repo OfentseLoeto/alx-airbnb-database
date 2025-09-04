8bb6a4755766e6461ef2e152c4784385bc2eccf5
Ofentse Loeto <ohentseloeto610@gmail.com>
2025-09-04 11:39:20 +0200

    Spelling errors fix

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS User (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    phone_number VARCHAR(20) NOT NULL,
-    role ENUM('gust', 'host', 'admim') NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    phone_number VARCHAR(20),
+    role ENUM('guest', 'host', 'admin') NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -39,11 +39,11 @@ CREATE TABLE IF NOT EXISTS Booking (
 -- Create Payment table
 CREATE TABLE IF NOT EXISTS Payment (
     payment_id VARCHAR(36) PRIMARY KEY,
-    booking_id VARCHAR(36) PRIMARY KEY,
+    booking_id VARCHAR(36) NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
     payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     payment_method ENUM('credit_card', 'paypal', 'stripe') NOT NULL,
-    FOREIGN KEY(booking_id) REFERENCES(booking_id)
+    FOREIGN KEY(booking_id) REFERENCES Booking(booking_id)
 );
 
 -- Create Review table
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS Review (
     review_id VARCHAR(36) PRIMARY KEY,
     property_id VARCHAR(36) NOT NULL,
     user_id VARCHAR(36) NOT NULL,
-    reating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    rating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
     comment TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (property_id) REFERENCES Property(property_id),
@@ -62,21 +62,21 @@ CREATE TABLE IF NOT EXISTS Review (
 CREATE TABLE IF NOT EXISTS Message (
     message_id VARCHAR(36) PRIMARY KEY,
     sender_id VARCHAR(36) NOT NULL,
-    recepient_id VARCHAR(36) NOT NULL,
+    recipient_id VARCHAR(36) NOT NULL,
     message_body TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (sender_id) REFERENCES User(user_id),
-    FOREIGN KEY (recepient_id) REFERENCES User(user_id)
+    FOREIGN KEY (recipient_id) REFERENCES User(user_id)
 );
 
 -- Create indexes for performence
-CREATE INDEX idx_user_email ON USER(email),
-CREATE INDEX idx_property_host ON Property(host_id),
-CREATE INDEX idx_booking_property ON Booking(property_id),
-CREATE INDEX idx_booking_user_id ON Booking(user_id),
-CREATE INDEX idx_payment_booking_id ON Payment(booking_id),
-CREATE INDEX idx_review_property_id ON Review(property_id),
-CREATE INDEX idx_review_user_id ON Review(user_id),
-CREATE INDEX idx_message_sender_id ON Message(sender_id),
+CREATE INDEX idx_user_email ON User(email);
+CREATE INDEX idx_property_host ON Property(host_id);
+CREATE INDEX idx_booking_property ON Booking(property_id);
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
+CREATE INDEX idx_payment_booking_id ON Payment(booking_id);
+CREATE INDEX idx_review_property_id ON Review(property_id);
+CREATE INDEX idx_review_user_id ON Review(user_id);
+CREATE INDEX idx_message_sender_id ON Message(sender_id);
 CREATE INDEX idx_message_recipient_id ON Message(recepient_id);
 
