e778d6f5df0f676f3a01d1d01e5b01c4002181f5
Ofentse Loeto <ohentseloeto610@gmail.com>
2025-09-04 10:07:06 +0200

    Created the performance Indexs

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -36,4 +36,47 @@ CREATE TABLE Booking (
     FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
 
--- 
+-- Create Payment table
+CREATE TABLE Payment (
+    payment_id VAARCHAR(36) PRIMARY KEY,
+    booking_id VARCHAR(36) PRIMARY KEY,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM('credit_cart', 'paypal', 'stripe') NOT NULL,
+    FOREIGN KEY(booking_id) REFERENCES(booking_id)
+);
+
+-- Create Review table
+CREATE TABLE Review (
+    review_id VARCHAR(36) PRIMARY KEY,
+    property_id VARCHAR(36) NOT NULL,
+    user_id VARCHAR(36) NOT NULL,
+    reating INT NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (property_id) REFERENCES Property(property_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+-- Create Message table
+CREATE TABLE Message (
+    message_id VARCHAR(36) PRIMARY KEY,
+    sender_id VARCHAR(36) NOT NULL,
+    recepient_id VARCHAR(36) NOT NULL,
+    message_body TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES User(user_id),
+    FOREIGN KEY (recepient_id) REFERENCES User(user_id)
+);
+
+-- Create indexes for performence
+CREATE INDEX idx_user_email ON USER(email),
+CREATE INDEX idx_property_host ON Property(host_id),
+CREATE INDEX idx_booking_property ON Booking(property_id),
+CREATE INDEX idx_booking_user_id ON Booking(user_id),
+CREATE INDEX idx_payment_booking_id ON Payment(booking_id),
+CREATE INDEX idx_review_property_id ON Review(property_id),
+CREATE INDEX idx_review_user_id ON Review(user_id),
+CREATE INDEX idx_message_sender_id ON Message(sender_id),
+CREATE INDEX idx_message_recipient_id ON Message(recepient_id);
+
