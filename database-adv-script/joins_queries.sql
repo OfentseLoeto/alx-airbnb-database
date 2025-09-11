ff2c2fa71f0bb6f030c5770936b510904ef50edb
Ofentse Loeto <ohentseloeto610@gmail.com>
2025-09-11 21:13:30 +0200

    added the file

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,51 @@
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM
+    Booking AS b
+INNER JOIN
+    Users AS u ON b.user_id = u.user_id;
+
+SELECT
+    p.property_id,
+    p.location,
+    p.pricepernight,
+    r.rating
+FROM
+    Property AS p
+LEFT JOIN
+    Review AS r ON p.property_id = r.property_id;
+
+SELECT
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
+    b.start_date,
+    b.end_date
+FROM
+    Users AS u
+LEFT JOIN
+    Booking AS b ON u.user_id = b.user_id
+
+UNION
+
+SELECT
+    u.user_id,
+    u.firt_name,
+    u.last_name,
+    u.email,
+    b.booking_id,
+    b.strart_date,
+    b.end_date
+FROM
+    Users AS u
+RIGHT JOIN
+    Booking AS b on u.user_id = b.user_id
+WHERE
+    u.user_id IS NULL;
