7d5d8f5272dec0092a0b8bfddb34144f74719ef3
Ofentse Loeto <ohentseloeto610@gmail.com>
2025-09-14 11:44:52 +0200

    Total number of bookigs per user (COUNT+GROUP)

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,23 @@
+--- Total number of bookigs per user (COUNT+GROUP)
+SELECT
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_booking
+FROM User u
+LEFT JOIN Booking b
+    ON u.user_id = b.user_id
+GROUP BY u.user_id, u.first_name, u.last_named
+ORDER BY total_booking DESC;
+
+--- Ranking properties by number of bookings (COUNT+GROUP+RANK) + (WINDOW FUNCTION)
+SELECT
+    p.property_id,
+    p.title,
+    COUNT(b.booking_id) AS total_booking,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM Property p
+LEFT JOIN Booking b
+    ON p.property_id = b.property_id
+GROUP BY p.property_id, p.title
+ORDER BY booking_rank;
