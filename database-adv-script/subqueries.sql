157dde2eef4f0f46f4a009f33ef6deb1bdfd4f7e
Ofentse Loeto <ohentseloeto610@gmail.com>
2025-09-14 10:54:02 +0200

    Find all properties that have an avarege rating greater than 4.0 and Find all users who have made more than 3 bookings

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,27 @@
+--- Find all properties that have an avarege rating greater than 4.0 ---
+SELECT
+    p.property_id,
+    p.title,
+    p.location
+FROM
+    Property p
+WHERE p.property_id IN (
+    SELECT r.property_id
+    FROM Review r
+    GROUP BY r.property_id
+    HAVING AVR(r.rating) > 4.0
+);
+
+--- Find all users who have made more than 3 bookings ---
+SELECT
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM
+    Users u
+WHERE (
+    SELECT COUNT(*)
+    FROM Booking b
+    WHERE b.user_id = u.user_id
+) > 3;
